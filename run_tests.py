a92438df23e51b0e12976f923534da38efb4c57a
xwan <3236624512@qq.com>
2026-06-09 06:30:32 +0000

    7.1.1

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-import sys
-import unittest
-
-sys.path.insert(0, "src")
-loader = unittest.TestLoader()
-suite = loader.loadTestsFromName("tests.test_smart_cache")
-runner = unittest.TextTestRunner(verbosity=2)
-result = runner.run(suite)
-sys.exit(0 if result.wasSuccessful() else 1)
